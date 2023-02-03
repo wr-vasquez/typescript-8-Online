26937dc83f2cf264e24322d21d2d7358e2b6864d
wr-vasquez <97376728+wr-vasquez@users.noreply.github.com>
2023-02-03 08:52:24 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-8-Online
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-8cbqh4)
---
+++ index.ts
@@ -0,0 +1,12 @@
+console.log('Hola mundo');
+
+const a: number = 10;
+
+console.log(a);
+
+// IMPORTANTE:
+// Ver la consola de JavaScript
+
+const nombre: string = 'Wilson';
+console.log(nombre);
+
