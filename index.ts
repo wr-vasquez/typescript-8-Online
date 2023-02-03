a8970c969d1ff07b776e695bc97b1bd8c54fe620
wr-vasquez <97376728+wr-vasquez@users.noreply.github.com>
2023-02-03 09:10:44 -0600

    1 file modified index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
-console.log('Hola mundo');
-
-const a: number = 10;
-
-console.log(a);
-
 // IMPORTANTE:
 // Ver la consola de JavaScript
 
-const nombre: string = 'Wilson';
-console.log(nombre);
+// const nombre: string = 'Wilson';
+// console.log(nombre);
 
+let habilidades: string[] = ['Bash', 'Counter', 'Healing'];
+
+//CREACTION OF INTERFACE
+interface Personaje {
+  nombre: string;
+  hp: number;
+  habilidades: string[];
+  puebloNatal: string;
+}
+
+const personaje: Personaje = {
+  nombre: 'Wilson',
+  hp: 72,
+  habilidades: ['Bash', 'Counter', 'Healing'],
+  puebloNatal: 'Texas',
+};
+
+console.log(personaje);
+
+console.log(personaje.nombre);
