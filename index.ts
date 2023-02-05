5ba513a4e0f60fa999dc809c3f67a5020b0c8947
wr-vasquez <97376728+wr-vasquez@users.noreply.github.com>
2023-02-05 17:00:27 -0600

    updated files

--- index.ts
+++ index.ts
@@ -6,7 +6,27 @@ interface Producto {
   precio: number;
 }
 
-const Telefono: Producto = {
+const telefono: Producto = {
   desc: 'Nokia',
   precio: 1200,
 };
+
+const tableta: Producto = {
+  desc: 'iPad Air',
+  precio: 1300,
+};
+
+function calcularISV(productos: Producto[]): number {
+  let total = 0;
+
+  productos.forEach(({ precio }) => {
+    total += precio;
+  });
+  return total * 0.15;
+}
+
+const articulos = [telefono, tableta];
+
+const isv = calcularISV(articulos);
+
+console.log('ISV: ', isv);
